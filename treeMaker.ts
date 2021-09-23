0ac71d405430d25e61afbc9bd68cbaebd13e6a5f
dofamine <pryatko.oleg@gmail.com>
2021-09-23 18:16:02 +0200

    Update treeMaker.ts

--- treeMaker.ts
+++ treeMaker.ts
@@ -11,12 +11,16 @@ interface IGroupedChildrenNodes {
 
 function getTreeStructureFromFlatFormat(flatNodes: ITreeNode[]): ITreeNode[] {
   const rootNodeKey: string = 'null';
-  const childrenNodes = flatNodes.reduce((acc, e) => {
+  const groupedChildrenNodesMap = getGroupedChildrenMap(flatNodes);
+  return getTreeWithChildren(groupedChildrenNodesMap[rootNodeKey], groupedChildrenNodesMap, Object.keys(groupedChildrenNodesMap));
+}
+
+function getGroupedChildrenMap(nodes: ITreeNode[]): IGroupedChildrenNodes {
+  return nodes.reduce((acc, e) => {
     const key = String(e.parentId);
     acc[key] = Array.isArray(acc[key]) ? [...acc[key], e] : [e];
     return acc;
   }, {} as IGroupedChildrenNodes);
-  return getTreeWithChildren(childrenNodes[rootNodeKey], childrenNodes, Object.keys(childrenNodes));
 }
 
 function getTreeWithChildren(nodes: ITreeNode[], childrenNodes: { [key: string]: ITreeNode[] }, groupIds: string[]): ITreeNode[] {
